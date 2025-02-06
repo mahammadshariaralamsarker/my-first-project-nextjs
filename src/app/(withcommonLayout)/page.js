9b59d0f1ff5f9c85b8993f16f3fe32c55870773c
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-02-06 11:01:31 +0600

    data load

--- src/app/(withcommonLayout)/page.js
+++ src/app/(withcommonLayout)/page.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = async() => {
-  const res = await fetch('http://localhost:5000/posts');
+  const res = await fetch('http://localhost:5000/posts',{
+    next:{
+      revalidate:5
+    }
+  });
   const posts = await res.json()
   console.log(posts);
   return (
@@ -12,6 +16,7 @@ const Home = async() => {
           <div className='flex'>
           <h1 className='text-black'>id:{item.id}</h1>
           <h1 key={item.id} className='text-black px-3'>{item.title}</h1>
+          <button>Buy now</button>
           </div>
           </>
         ))
